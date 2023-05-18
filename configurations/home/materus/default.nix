c635de13f3feb4bea810a040d03f9e7d7b7194de
materus <materus@podkos.pl>
2023-05-18 21:57:42 +0200

    Move home settings to be set per host

--- configurations/home/materus/default.nix
+++ configurations/home/materus/default.nix
@@ -1,22 +1,10 @@
 { config, pkgs, lib, ... }:
 {
 
-  materus.profile = {
-    fonts.enable = lib.mkDefault true;
-    nixpkgs.enable = lib.mkDefault true;
-    enableDesktop = lib.mkDefault true;
-    enableTerminal = lib.mkDefault true;
-    enableTerminalExtra = lib.mkDefault true;
-    enableNixDevel = lib.mkDefault true;
-
-  };
-  programs.git.signing.key = "28D140BCA60B4FD1";
-  programs.git.signing.signByDefault = true;
-  programs.git.userEmail = "materus@podkos.pl";
-  programs.git.userName = "materus";
-
   home.username =  "materus";
-  home.homeDirectory = "/home/materus";
-  home.stateVersion =  "23.05";
   home.packages = [pkgs.papirus-icon-theme];
+
+  programs.git.signing.key = lib.mkDefault "28D140BCA60B4FD1";
+  programs.git.userEmail = lib.mkDefault "materus@podkos.pl";
+  programs.git.userName = lib.mkDefault "materus";
 }
--- configurations/host/materusPC/extraHome.nix
+++ configurations/host/materusPC/extraHome.nix
@@ -1,1 +1,9 @@
-{}
+{ config, pkgs, lib, ... }:
+{
+  home.stateVersion = "23.05";
+  home.homeDirectory = "/home/materus";
+  programs.git.signing.key = "28D140BCA60B4FD1";
+  programs.git.signing.signByDefault = true;
+  programs.git.userEmail = "materus@podkos.pl";
+  programs.git.userName = "materus";
+}
